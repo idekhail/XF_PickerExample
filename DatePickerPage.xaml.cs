94ae30bff11e1baac86fb5866856602e499708cb
Ibrahim AlDekhail <33824044+idekhail@users.noreply.github.com>
2023-10-11 00:21:13 +0300

    Add files via upload

---
+++ DatePickerPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XF_PickerExample
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DatePickerPage : ContentPage
+    {
+        public DatePickerPage()
+        {
+            InitializeComponent();
+
+        }
+
+
+        private void btnShow_Clicked(object sender, EventArgs e)
+        {
+            DateTime date = datePicker.Date;
+            var time = timePicker.Time;
+
+            string datetime = "Date: " + date.ToString("d") + " time is: " + time.ToString();
+            lblResult.Text = datetime;
+        }
+
+
+    }
+}
