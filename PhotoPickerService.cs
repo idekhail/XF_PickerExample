54594337ac43060e22496fbc3319131461594b8f
Ibrahim AlDekhail <33824044+idekhail@users.noreply.github.com>
2023-10-15 02:44:14 +0300

    Add files via upload

---
+++ PhotoPickerService.cs
@@ -0,0 +1,45 @@
+﻿
+
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+using Windows.Storage;
+using Windows.Storage.Pickers;
+using Windows.Storage.Streams;
+
+using Xamarin.Forms;
+
+using XF_PickerExample.UWP;
+
+[assembly: Dependency(typeof(PhotoPickerService))]
+namespace XF_PickerExample.UWP
+{
+    public class PhotoPickerService : IPhotoPickerService
+    {
+        public async Task<Stream> GetImageStreamAsync()
+        {
+            // Create and initialize the FileOpenPicker
+            FileOpenPicker openPicker = new FileOpenPicker
+            {
+                ViewMode = PickerViewMode.Thumbnail,
+                SuggestedStartLocation = PickerLocationId.PicturesLibrary,
+            };
+
+            openPicker.FileTypeFilter.Add(".jpg");
+            openPicker.FileTypeFilter.Add(".jpeg");
+            openPicker.FileTypeFilter.Add(".png");
+
+            // Get a file and return a Stream
+            StorageFile storageFile = await openPicker.PickSingleFileAsync();
+
+            if (storageFile == null)
+            {
+                return null;
+            }
+
+            IRandomAccessStreamWithContentType raStream = await storageFile.OpenReadAsync();
+            return raStream.AsStreamForRead();
+        }
+    }    
+}
