a9355470658f6fb195438d5b2484faf60634c3ee
Ibrahim AlDekhail <33824044+idekhail@users.noreply.github.com>
2023-10-15 02:43:31 +0300

    Add files via upload

---
+++ IPhotoPickerService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XF_PickerExample
+{
+    public interface IPhotoPickerService
+    {
+        Task<Stream> GetImageStreamAsync();
+    }
+}
